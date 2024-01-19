082e9e9173023b27a9bf647adb2b7e941672f7e7
Annabella <eliisaannaloiv@gmail.com>
2024-01-19 15:09:38 +0200

    data inside looking better

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,20 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props){
-    console.log(props)
+    
+    const day = props.expenseData.date.toLocaleString
+        ('en-US',{day: '2-digit'})
+    const month = props.expenseData.date.toLocaleString
+        ('en-US',{month: 'long'})
+    const year = props.expenseData.date.getFullYear()
 
 
     return (
         <div className="expense-item">
-            <div>{props.expenseData.date.toString()}</div>
+            <ExpenseDate
+                date={props.expenseData.date} 
+            />
             <div className='expense-item__description'>
                 <h2>{props.expenseData.title}</h2>
                 <div className='expense-item__price'>{props.expenseData.price}</div>
