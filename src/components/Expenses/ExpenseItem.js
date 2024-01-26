513b6fd993d0fa3963b4610da8497d405e597025
Annabella <eliisaannaloiv@gmail.com>
2024-01-26 15:19:26 +0200

    lisasin click me nupu

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,8 +2,16 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Expenses from './Expenses'
 import Card from '../UI/Card'
+import { useState } from 'react' 
 
-function ExpenseItem(props){
+const ExpenseItem = (props) =>{
+    const[title, setTitle] = useState(props.expenseData.title) 
+
+    const clickHandler = () =>{
+        console.log('YOU CLICKED!')
+        setTitle(`Updated by click ${title}`)
+        console.log(title)
+    } 
 
     return (
         <div className="expense-item">
@@ -14,6 +22,7 @@ function ExpenseItem(props){
                 <h2>{props.expenseData.title}</h2>
                 <div className='expense-item__price'>{props.expenseData.price}</div>
             </div>
+            <button onClick={clickHandler}>Click!</button>
         </div>
     )
 } 
